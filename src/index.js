5233a8beee5656a19aa54b0ae2daab8071322925
Yauheni Saladukha <evgennn32@gmail.com>
2021-03-11 14:42:46 +0300

    init: add min max avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+    if( array != undefined  && array.length > 0 ) {
+        let min = Math.min(...array);
+        if (min && !isNaN(min)) {
+            return min;
+        }
+    }
+    return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if( array != undefined  && array.length > 0 ) {
+        let max = Math.max(...array);
+        if (max && !isNaN(max)) {
+            return max;
+        }
+    }
+    return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if( array != undefined  && array.length > 0 ){
+        let total = array.reduce((a,b)=> a+b);
+        if(total){
+            return total/array.length;
+        }
+    }
+
+    return 0;
 }
